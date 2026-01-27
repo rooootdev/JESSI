5a74893e381c8d2be036e6e03073141915b2c10a
BaconMania <plurp943@gmail.com>
2026-01-26 20:17:39 -0800

    Initial Commit

---
+++ JESSI/JESSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JESSIApp.swift
+//  JESSI
+//
+//  Created by BaconMania on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct JESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
